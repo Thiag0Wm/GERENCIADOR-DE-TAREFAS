97343d259bddc23e2e19a6b12fc5ee75cf7e0049
Thiago welington <thiagowm2007@gmail.com>
2026-08-23 00:15:31 -0300

    agr tem localStoraage

--- script.js
+++ script.js
@@ -1,35 +1,61 @@
 const formulario = document.querySelector("#task-form");
 const campoTarefa = document.querySelector("#task-input");
 const listaTarefas = document.querySelector("#task-list");
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
+function criarTarefaNaTela(dadosDaTarefa) {
+    const novaTarefa = document.createElement("li");
+    novaTarefa.textContent = dadosDaTarefa.texto;
+    novaTarefa.classList.toggle("concluida", dadosDaTarefa.concluida);
+
+    listaTarefas.appendChild(novaTarefa);
+    const botaoExcluir = document.createElement("button");
+    botaoExcluir.textContent = "Excluir";
+    novaTarefa.appendChild(botaoExcluir);
+
+    botaoExcluir.addEventListener("click", function (eventoClique) {
+        eventoClique.stopPropagation();
+        novaTarefa.remove();
+        const indice = tarefas.indexOf(dadosDaTarefa);
+        if (indice !== -1) {
+            tarefas.splice(indice, 1);
+            localStorage.setItem("tarefas", JSON.stringify(tarefas));
+        }
+    });
+
+    novaTarefa.addEventListener("click", function () {
+        dadosDaTarefa.concluida = !dadosDaTarefa.concluida;
+        novaTarefa.classList.toggle("concluida", dadosDaTarefa.concluida);
+        localStorage.setItem("tarefas", JSON.stringify(tarefas));
+    });
+}
+
+    tarefas.forEach(function (dadosDaTarefa) {
+        criarTarefaNaTela(dadosDaTarefa);
+    });
 
 formulario.addEventListener("submit", function(evento){
+    
+    
     evento.preventDefault();
+
+    
 
     const textoTarefa = campoTarefa.value.trim();
 
     if (textoTarefa === ""){
         return;
-    }
-    
-   
-    const novaTarefa = document.createElement("li");
-    novaTarefa.textContent = textoTarefa;
-
-    listaTarefas.appendChild(novaTarefa); 
-    const botaoExcluir = document.createElement("button");
-    botaoExcluir.textContent = "Excluir";
-    novaTarefa.appendChild(botaoExcluir);
-    campoTarefa.value = "";
-    
-    botaoExcluir.addEventListener("click", function (eventoClique) {
-        eventoClique.stopPropagation();
-        novaTarefa.remove();
         
-    });
-    novaTarefa.addEventListener("click", function () {
-        novaTarefa.classList.toggle("concluida");
-    });
-    
-    
+    }
+    const dadosDaTarefa = {
+    texto: textoTarefa,
+    concluida: false
+};   
+    tarefas.push(dadosDaTarefa);
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+
+    criarTarefaNaTela(dadosDaTarefa);
+    campoTarefa.value = "";
+
+    console.log(tarefas);
 });
